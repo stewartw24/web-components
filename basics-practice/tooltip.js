20dbe1433e09a7fee63c238cecd0886282ad96f1
William <stewart-w24@hotmail.co.uk>
2022-01-16 13:34:15 +0000

    changed logic to use a render method

--- basics-practice/tooltip.js
+++ basics-practice/tooltip.js
@@ -1,8 +1,8 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    this._tooltipContainer;
     this._tooltipIcon;
+    this._tooltipVisible = false;
     this._tooltipText = "Some dummy tooltip text";
     this.attachShadow({ mode: "open" });
     // const template = document.querySelector('#tooltip-template');
@@ -54,48 +54,68 @@ class Tooltip extends HTMLElement {
 
   connectedCallback() {
     if (this.hasAttribute("tooltip-text")) {
-      console.log(this.getAttribute("tooltip-text"));
+      // console.log(this.getAttribute("tooltip-text"));
       this._tooltipText = this.getAttribute("tooltip-text");
     }
     this._tooltipIcon = this.shadowRoot.querySelector("span");
-    this._tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
-    this._tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    this._tooltipIcon.addEventListener(
+      "mouseenter",
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      "mouseleave",
+      this._hideTooltip.bind(this)
+    );
     // this.shadowRoot.appendChild(tooltipIcon);
     this.style.position = "relative";
+    this._render();
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if(oldValue === newValue){
+    if (oldValue === newValue) {
       return;
     }
-    if(name === 'text'){
+    if (name === "text") {
       this._tooltipText = newValue;
     }
   }
 
-  static get observedAttributes(){
-    return ['text'];
+  static get observedAttributes() {
+    return ["text"];
   }
 
-  disconnectCallback(){
-    console.log('disconnected');
+  disconnectCallback() {
+    // console.log("disconnected");
     //cleanup event listeners if the elements are removed
-    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
-    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+    this._tooltipIcon.removeEventListener("mouseenter", this._showTooltip);
+    this._tooltipIcon.removeEventListener("mouseleave", this._hideTooltip);
+  }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector("div");
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement("div");
+      tooltipContainer.textContent = this._tooltipText;
+      // this._tooltipContainer.style.backgroundColor = 'black';
+      // this._tooltipContainer.style.color = 'white';
+      // this._tooltipContainer.style.position = 'absolute';
+      // this._tooltipContainer.style.zIndex = '10';
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
   }
 
   _showTooltip() {
-    this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = this._tooltipText;
-    // this._tooltipContainer.style.backgroundColor = 'black';
-    // this._tooltipContainer.style.color = 'white';
-    // this._tooltipContainer.style.position = 'absolute';
-    // this._tooltipContainer.style.zIndex = '10';
-    this.shadowRoot.appendChild(this._tooltipContainer);
+    this._tooltipVisible = true;
+    this._render();
   }
 
   _hideTooltip() {
-    this.shadowRoot.removeChild(this._tooltipContainer);
+    this._tooltipVisible = false;
+    this._render();
   }
 }
 
