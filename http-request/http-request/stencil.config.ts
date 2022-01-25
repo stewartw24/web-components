85852567a0eb3d6df44d6e8801314dfd62182f22
William <stewart-w24@hotmail.co.uk>
2022-01-25 18:30:52 +0000

    http request practice

---
+++ http-request/http-request/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'http-request',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
