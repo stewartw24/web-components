5419521d1df9365a691de048adab519d6f114b70
William <stewart-w24@hotmail.co.uk>
2023-01-29 15:32:46 +0000

    adding web component practice projects

--- http-request/http-request/src/components/stock-finder/stock-finder.tsx
+++ http-request/http-request/src/components/stock-finder/stock-finder.tsx
@@ -1,61 +1,65 @@
 import { Component, h, State, Event, EventEmitter } from '@stencil/core';
 
-import { AV_API_KEY } from '../../global/global'
+import { AV_API_KEY } from '../../global/global';
 
 @Component({
-    tag: 'ws-stock-finder',
-    styleUrl: './stock-finder.css',
-    shadow: true,
+  tag: 'ws-stock-finder',
+  styleUrl: './stock-finder.css',
+  shadow: true, 
 })
 export class StockFinder {
-    stockNameInput: HTMLInputElement;
+  stockNameInput: HTMLInputElement;
 
-    @State() searchResults: {symbol: string, name: string}[] = [];
-    @State() loading = false;
+  @State() searchResults: { symbol: string; name: string }[] = [];
+  @State() loading = false;
 
-    @Event({bubbles: true, composed: true}) wsSymbolSelected: EventEmitter<string>;
+  @Event({ bubbles: true, composed: true }) wsSymbolSelected: EventEmitter<string>;
 
-    onFindStocks(event: Event){
-        event.preventDefault();
-        this.loading = true;
-        const stockName = this.stockNameInput.value;
-        fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
-        .then(res => res.json())
-        .then(parsedRes => {
-            this.searchResults = parsedRes['bestMatches'].map(match => {
-                return {name: match['2. name'], symbol: match['1. symbol']};
-            });
-            this.loading = false;
-        })
-        .catch(err => {
-            console.error(err);
-            this.loading = false;
+  onFindStocks(event: Event) {
+    
+    event.preventDefault();
+    this.loading = true;
+    const stockName = this.stockNameInput.value;
+    fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
+      .then(res => res.json())
+      .then(parsedRes => {
+        this.searchResults = parsedRes['bestMatches'].map(match => {
+          return { name: match['2. name'], symbol: match['1. symbol'] };
         });
-    }
+        this.loading = false;
+      })
+      .catch(err => {
+        console.error(err);
+        this.loading = false;
+      });
+  }
 
-    //emit your own custom event
-    onSelectSymbol(symbol: string){
-        //emit is a simplified way of dispatching the event
-        //emitting so we can listen inside another component.
-        this.wsSymbolSelected.emit(symbol);
-    }
+  //emit your own custom event
+  onSelectSymbol(symbol: string) {
+    //emit is a simplified way of dispatching the event
+    //emitting so we can listen inside another component.
+    this.wsSymbolSelected.emit(symbol);
+  }
 
-    render(){
-        let content = <ul>{this.searchResults.map(result => (
-            <li onClick={this.onSelectSymbol.bind(this, result.symbol)}><strong>{result.symbol}</strong> - {result.name}</li>
-            ))}</ul>;
-        if(this.loading){
-            content = <ws-spinner />
-        }
-        return [
-            <form onSubmit={this.onFindStocks.bind(this)}>
-                <input 
-                    id="stock-symbol" 
-                    ref={el => (this.stockNameInput = el)} 
-                />
-                <button type="submit">Find</button>
-            </form>,
-            content
-        ];
+  render() {
+    let content = (
+      <ul>
+        {this.searchResults.map(result => (
+          <li onClick={this.onSelectSymbol.bind(this, result.symbol)}>
+            <strong>{result.symbol}</strong> - {result.name}
+          </li>
+        ))}
+      </ul>
+    );
+    if (this.loading) {
+      content = <ws-spinner />;
     }
+    return [
+      <form onSubmit={this.onFindStocks.bind(this)}>
+        <input id="stock-symbol" ref={el => (this.stockNameInput = el)} />
+        <button type="submit">Find</button>
+      </form>,
+      content,
+    ];
+  }
 }
---
+++ react-test/react-test/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <bpor-widget distance="20" query="cancer" location="Belfast" />
+    </div>
+  );
+}
+
+export default App;
